228bafc1940fd7b80b9fe03d51c0a2e60c95f4b6
Ellisys <gustavo_falv@hotmail.com>
2021-02-09 03:09:46 -0300

    SignUp block add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var signIn = document.getElementById("btnSignIn");
 var signUp = document.getElementById("btnSignUp");
-
+var signUpBlock = document.getElementById("campo2");
 signIn.addEventListener("click", ()=>{
     
 })
 
 signUp.addEventListener("click", ()=>{
-    
+    signUpBlock.style.bottom = "224px";
 })
