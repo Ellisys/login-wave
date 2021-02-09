7daa2b19957f8aab95e837942aab45ee9e358b30
Ellisys <gustavo_falv@hotmail.com>
2021-02-09 03:00:24 -0300

    Add js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var signIn = document.getElementById("btnSignIn");
+var signUp = document.getElementById("btnSignUp");
+
+signIn.addEventListener("click", ()=>{
+    
+})
+
+signUp.addEventListener("click", ()=>{
+    
+})
