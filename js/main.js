795415ae1b146a76dd54cbdf5e9f4166f049d875
Ellisys <gustavo_falv@hotmail.com>
2021-02-11 20:30:03 -0300

    Registro adicionado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var signIn = document.getElementById("btnSignIn");
+var signIn = document.getElementById("btnSignInCad");
 var signUp = document.getElementById("btnSignUp");
 var signUpBlock = document.getElementById("campo2");
 signIn.addEventListener("click", ()=>{
@@ -6,5 +6,30 @@ signIn.addEventListener("click", ()=>{
 })
 
 signUp.addEventListener("click", ()=>{
-    signUpBlock.style.bottom = "224px";
+
+    if(window.screen.availWidth < 768){
+        signUpBlock.style.bottom = "140px";
+    }
+
+    if(window.screen.availWidth >= 768){
+        signUpBlock.style.bottom = "220px";
+    }
+
+    if(window.screen.availWidth >= 1024){
+        signUpBlock.style.bottom = "160px";
+    }
+    
+    if(window.screen.availWidth >= 1440){
+        signUpBlock.style.bottom = "328px";
+    }
+
+    
+
 })
+
+signIn.addEventListener("click", ()=>{
+    signUpBlock.style.transition = "2s";
+    signUpBlock.style.bottom = "-328px";
+})
+
+console.log(window.screen.availWidth)
